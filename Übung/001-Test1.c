3599a7d0ffa9df78cfead43d532c576aaff99f65
Lukas Mayr <145333289+GimliTV323@users.noreply.github.com>
2023-10-05 16:12:54 +0000

    Var Test

---
+++ Übung/001-Test1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+  int var1 = 12;
+
+  int var2 = 33;
+
+  int var3 = 69;
+
+  int var4 = 102283;
+
+  int var5 = 1278;
+
+  printf("%4d\n", var1 + var2 - var3 + var4 - var5);
+
+
+
+
+return 0;
+}
